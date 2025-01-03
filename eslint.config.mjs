6c9fcd14e106247818f9b178184f70cf59e76096
pradeeprai1990 <pradeep.9997@gmail.com>
2025-01-03 13:26:37 +0530

    Cahnegs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,10 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+module.exports = {
+  extends: ["next/core-web-vitals"],
+};
+
 const eslintConfig = [...compat.extends("next/core-web-vitals")];
 
 export default eslintConfig;
